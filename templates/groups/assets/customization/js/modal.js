ba3264a317837e84484488571809f215cafa61bf
iamblackornot <nkzshinnnik@gmail.com>
2022-02-26 15:06:38 +0300

    - fixed closing and stopping youtube iframe

--- templates/groups/assets/customization/js/modal.js
+++ templates/groups/assets/customization/js/modal.js
@@ -1,6 +1,7 @@
 var modal = $('#videoModal');
 var openBtn = $('#open-video-modal');
 var closeBtn = $('#close-video-modal');
+var iframe = $('#youtube-frame');
 
 openBtn.on('click', () => {
     modal.css('display', 'block');
@@ -8,11 +9,17 @@ openBtn.on('click', () => {
 
 closeBtn.on('click', () => {
     modal.css('display', 'none');
+    stopVideo();
 });
 
 window.onclick = function(event) {
-    
     if (event.target == modal[0]) {
         modal.css('display', 'none');
+        stopVideo();
     }
+}
+
+function stopVideo() {
+    var src = iframe.attr('src');
+    iframe.attr('src', src);
 }
